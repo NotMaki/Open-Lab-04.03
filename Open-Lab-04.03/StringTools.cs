cdd19e223f29d581ddd6df58bdc8310d926f0f99
Marko <jantulamarko@outlook.com>
2022-10-06 07:55:20 +0200

    Update StringTools.cs

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
+using System;
 namespace Open_Lab_04._03
 {
     public class StringTools
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length > 2)
+            {
+                return original.Substring(1, original.Length - 2);
+            }
+
+
+            else if (original.Length < 2 && original.Length > 0)
+            {
+                return original;
+            }
+
+            else
+                return String.Empty;
         }
     }
 }
